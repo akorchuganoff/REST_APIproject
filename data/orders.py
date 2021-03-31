0cad741640b8bf444f4eec101a0585058a962f2d
akorchuganoff <akorchuganoff@yandex.ru>
2021-03-31 16:41:36 +0700

    orders table

---
+++ data/orders.py
@@ -0,0 +1,18 @@
+import datetime
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+from sqlalchemy_serializer import SerializerMixin
+
+
+class Courier(SqlAlchemyBase, SerializerMixin):
+    __tablename__ = 'Couriers'
+
+    order_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    weight = sqlalchemy.Column(sqlalchemy.Float, nullable=False)
+    region = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    delivery_hours = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+
+
+    def __repr__(self):
+        return [{'Order_id': self.order_id}, {'Weight': self.weight}, {'Region': self.region},
+                {'Delivery_hours hours': self.delivery_hours}]
