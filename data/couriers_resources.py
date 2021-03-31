925cc593b21a73fd4c8c67d5100518ac529ea77b
akorchuganoff <akorchuganoff@yandex.ru>
2021-03-31 13:27:34 +0700

    try bd in module

--- data/couriers_resources.py
+++ data/couriers_resources.py
@@ -7,6 +7,7 @@ parser = reqparse.RequestParser()
 parser.add_argument('id', required=False, type=int)
 parser.add_argument('data', required=False, action='append') # для примера
 
+db_sess = db_session.create_session()
 
 def abort_if_not_found(id):
         abort(404, message=f"courier {id} not found") # функция для вызова ошибки и возврата message
@@ -14,7 +15,8 @@ def abort_if_not_found(id):
 
 class CourierResource(Resource):
     def get(self, id):
-        return jsonify({'success': 'OK'})# тут какая-то твоя работа функции которая возвращает json
+        user = db_sess.query(Courier).filter(Courier.courier_id == id)
+        return jsonify(user)# тут какая-то твоя работа функции которая возвращает json
 
     def delete(self, id):
         return jsonify({'success': 'OK'}) # на удаление примитив ok
